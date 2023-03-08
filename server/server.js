3f393ca07a0ccedf60d6522d64b12cbea379353b
Andrew <andrewg87@gmail.com>
2023-03-08 13:50:07 -0500

    minor change

--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ const startApolloServer = async(typeDefs,resolvers)=>{
       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
-};
+}
 
 startApolloServer(typeDefs,resolvers);
